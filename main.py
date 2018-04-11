2e1cfe0b2dd0577969d150d503e56622ddd8b4d1
ViDurand <34512615+ViDurand@users.noreply.github.com>
2018-04-11 21:41:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,21 +2,48 @@
 import numpy as np
 import pandas as pd
 
-
-
-#%%
-if __name__ == "__main__":
-    df = pd.read_csv('../dataset/part_1_1/part_1_1__Ground_Truth.tsv', sep='\t', header=0)
+def fromCsvToDict_forGT(csv_path):
+    df = pd.read_csv(csv_path, sep='\t', header=0)
     keys = list(df['Query_id'])
     values = list(df['Relevant_Doc_id'])
-    gt = dict() #ground truth dictionary set to empty to each query_id correspond a list of relevant docss
+    dic = dict() #ground truth dictionary set to empty to each query_id correspond a list of relevant docss
     
     i = 0
     for v in values:
-        if keys[i] in gt.keys():
-            gt[keys[i]].append(v)
+        if keys[i] in dic.keys():
+            dic[keys[i]].append(v)
         else:
-            gt[keys[i]] = []
-            gt[keys[i]].append(v)
+            dic[keys[i]] = []
+            dic[keys[i]].append(v)
         i = i+1
+        
+    return dic
+
+def fromCsvToDict_forSE(csv_path):
+    df = pd.read_csv(csv_path, sep='\t', header=0)
+    keys = list(df['Query_ID'])
+    doc_ids = list(df['Doc_ID'])
+    ranks = list(df['Rank'])
+    dic = dict() #ground truth dictionary set to empty to each query_id correspond a list of relevant docss
+    
+    for i in range(len(keys)):
+        if keys[i] in dic.keys():
+            dic[keys[i]].append((doc_ids[i], ranks[i]))
+        else:
+            dic[keys[i]] = []
+            dic[keys[i]].append((doc_ids[i], ranks[i]))
+        i = i+1
+        
+    return dic
 #%%
+if __name__ == "__main__":
+    ground_truth_path = '../dataset/part_1_1/part_1_1__Ground_Truth.tsv'
+    Results_SE_1_path = '../dataset/part_1_1/part_1_1__Results_SE_1.tsv'
+    Results_SE_2_path = '../dataset/part_1_1/part_1_1__Results_SE_2.tsv'
+    Results_SE_3_path = '../dataset/part_1_1/part_1_1__Results_SE_3.tsv'
+    
+    gt_part1 = fromCsvToDict_forGT(ground_truth_path)
+    se1_part1 = fromCsvToDict_forSE(Results_SE_1_path)
+    
+    
+#%%s
